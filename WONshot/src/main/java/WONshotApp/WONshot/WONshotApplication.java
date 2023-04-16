86877c0d4cdf95a696227e3373455403d3b9bf91
yeonji <yjuio8357@naver.com>
2023-04-16 20:51:07 +0900

    initial commit

---
+++ WONshot/src/main/java/WONshotApp/WONshot/WONshotApplication.java
@@ -0,0 +1,13 @@
+package WONshotApp.WONshot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WONshotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WONshotApplication.class, args);
+	}
+
+}
---
+++ WONshot/src/test/java/WONshotApp/WONshot/WONshotApplicationTests.java
@@ -0,0 +1,13 @@
+package WONshotApp.WONshot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WONshotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
