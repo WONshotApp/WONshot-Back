0e38cbe58f83379db429b7c6e90e3b1063185a17
hyerimmy <heyrim2010@naver.com>
2023-04-30 22:21:47 +0900

    feat : domain 정의 코드 추가

---
+++ WONshot/src/main/java/WONshotApp/WONshot/domain/ExpenseCategoryType.java
@@ -0,0 +1,12 @@
+package WONshotApp.WONshot.domain;
+
+
+public enum ExpenseCategoryType {
+    FOOD, //식품
+    FASHION, //의류,집화
+    LIVING, //생필품
+    BEAUTY, //화장품
+    ELECTRONICS, //가전
+    HEALTH, //건강,의료
+    ETC, //기타
+}
---
+++ WONshot/src/main/java/WONshotApp/WONshot/domain/IncomeCategoryType.java
@@ -0,0 +1,8 @@
+package WONshotApp.WONshot.domain;
+
+
+public enum IncomeCategoryType {
+    SALARY, //급여
+    INTEREST, //이자
+    ETC, //기타
+}
