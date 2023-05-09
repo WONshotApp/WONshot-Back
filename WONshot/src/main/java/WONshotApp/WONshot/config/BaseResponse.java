8287b8d585fd95871962970769494c5030fb9219
hyerimmy <heyrim2010@naver.com>
2023-05-10 01:31:04 +0900

    feat : base response 구조 생성

---
+++ WONshot/src/main/java/WONshotApp/WONshot/config/BaseResponse.java
@@ -0,0 +1,33 @@
+package WONshotApp.WONshot.config;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+import static WONshotApp.WONshot.config.BaseResponseStatus.SUCCESS;
+
+@Getter
+@AllArgsConstructor
+public class BaseResponse<T> {
+    private final int code;
+    private final HttpStatus httpStatus;
+    private final String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T result;
+
+    // 요청에 성공한 경우
+    public BaseResponse(T result) {
+        this.code = SUCCESS.getCode();
+        this.httpStatus = SUCCESS.getHttpStatus();
+        this.message = SUCCESS.getMessage();
+        this.result = result;
+    }
+
+    // 요청에 실패한 경우
+    public BaseResponse(BaseResponseStatus status) {
+        this.code = status.getCode();
+        this.httpStatus = status.getHttpStatus();
+        this.message = status.getMessage();
+    }
+}
