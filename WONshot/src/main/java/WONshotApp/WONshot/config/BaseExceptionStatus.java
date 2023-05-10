8c7a10d3f90592172ae0d1a02133e413f2a78f69
hyerimmy <heyrim2010@naver.com>
2023-05-11 01:08:13 +0900

    feat : 예외처리 구조화(base exception)

--- WONshot/src/main/java/WONshotApp/WONshot/config/BaseExceptionStatus.java
+++ WONshot/src/main/java/WONshotApp/WONshot/config/BaseExceptionStatus.java
@@ -4,16 +4,16 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 @Getter
-public enum BaseResponseStatus {
-    /* ~1000 : success */
-    SUCCESS(200, HttpStatus.OK, "요청에 성공하였습니다.");
+public enum BaseExceptionStatus {
+    /* 1000 : user exception */
+    EMPTY_EMAIL(1001, HttpStatus.BAD_REQUEST, "이메일이 빈 값 입니다.");
 
 
     private final int code;
     private final HttpStatus httpStatus;
     private final String message;
 
-    BaseResponseStatus(int code, HttpStatus httpStatus, String message) {
+    BaseExceptionStatus(int code, HttpStatus httpStatus, String message) {
         this.code = code;
         this.httpStatus = httpStatus;
         this.message = message;
