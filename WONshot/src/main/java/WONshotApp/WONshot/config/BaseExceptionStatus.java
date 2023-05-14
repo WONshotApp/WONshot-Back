6cf3d5683729c3762f22f9c2ac7634fb3d7a0537
hyerimmy <heyrim2010@naver.com>
2023-05-15 00:18:43 +0900

    chore : 회원가입 이메일, id, password, 닉네임 정규표현 검증 코드 완료

--- WONshot/src/main/java/WONshotApp/WONshot/config/BaseExceptionStatus.java
+++ WONshot/src/main/java/WONshotApp/WONshot/config/BaseExceptionStatus.java
@@ -8,8 +8,15 @@ public enum BaseExceptionStatus {
     /**
      * *User Exception* 1000 ~ 1999
      */
-    EMPTY_EMAIL(1001, HttpStatus.BAD_REQUEST, "이메일을 입력해주세요."),
-    INVALID_EMAIL(1002, HttpStatus.BAD_REQUEST , "이메일 형식이 올바르지 않습니다.");
+    EMPTY_EMAIL(1001, HttpStatus.BAD_REQUEST, "이메일 값이 비어있습니다."),
+    INVALID_EMAIL(1002, HttpStatus.BAD_REQUEST , "이메일 형식이 올바르지 않습니다."),
+    EMPTY_NICKNAME(1003, HttpStatus.BAD_REQUEST, "닉네임 값이 비어있습니다."),
+    INVALID_NICKNAME(1004, HttpStatus.BAD_REQUEST , "닉네임 형식이 올바르지 않습니다."),
+    EMPTY_ID(1005, HttpStatus.BAD_REQUEST, "아이디 값이 비어있습니다."),
+    INVALID_ID(1006, HttpStatus.BAD_REQUEST , "아이디 형식이 올바르지 않습니다."),
+    EMPTY_PASSWORD(1007, HttpStatus.BAD_REQUEST, "비밀번호 값이 비어있습니다."),
+    INVALID_PASSWORD(1008, HttpStatus.BAD_REQUEST , "비밀번호 형식이 올바르지 않습니다."),
+    ;
 
 
 
--- WONshot/src/main/java/WONshotApp/WONshot/service/UserService.java
+++ WONshot/src/main/java/WONshotApp/WONshot/service/UserService.java
@@ -18,7 +18,8 @@ import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
-import static WONshotApp.WONshot.utils.ValidationRegex.isRegexEmail;
+import static WONshotApp.WONshot.config.BaseExceptionStatus.INVALID_PASSWORD;
+import static WONshotApp.WONshot.utils.ValidationRegex.*;
 
 @Service
 @RequiredArgsConstructor
@@ -40,40 +41,26 @@ public class UserService implements UserDetailsService {
         if (!isRegexEmail(joinUserReq.getEmail())) {
             throw new BaseException(BaseExceptionStatus.INVALID_EMAIL);
         }
-//        //id 정규표현: 입력받은 id가 영문 대소문자,숫자 4-16자리 형식인지 검사합니다. 형식이 올바르지 않다면 에러 메시지를 보냅니다.
-//        if (!isRegexId(postUserReq.getUserId())) {
-//            return new BaseResponse<>(POST_USERS_INVALID_ID);
-//        }
-//        // password에 값이 존재하는지, 빈 값으로 요청하지는 않았는지 검사합니다. 빈값으로 요청했다면 에러 메시지를 보냅니다.
-//        if (postUserReq.getUserPwd().length() == 0) {
-//            return new BaseResponse<>(POST_USERS_EMPTY_PASSWORD);
-//        }
-//        //비밀번호 정규표현: 입력받은 비밀번호가 숫자, 특문 각 1회 이상, 영문은 대소문자 모두 사용하여 8~16자리 입력과 같은 형식인지 검사합니다. 형식이 올바르지 않다면 에러 메시지를 보냅니다.
-//        if (!isRegexPassword(postUserReq.getUserPwd())) {
-//            return new BaseResponse<>(POST_USERS_INVALID_PASSWORD);
-//        }
-//        // 닉네임 값이 존재하는지, 빈 값으로 요청하지는 않았는지 검사합니다. 빈값으로 요청했다면 에러 메시지를 보냅니다.
-//        if (postUserReq.getUserNickname().length() == 0) {
-//            return new BaseResponse<>(POST_USERS_EMPTY_NICKNAME);
-//        }
-//        // 닉네임 정규표현: 입력받은 닉네임이 숫자와 영문, 한글로만 이루어졌는지 검사합니다. 형식이 올바르지 않다면 에러 메시지를 보냅니다.
-//        if (!isRegexNickname(postUserReq.getUserNickname())) {
-//            return new BaseResponse<>(POST_USERS_INVALID_NICKNAME);
-//        }
-//        // 주소가 존재하는지, 빈 값으로 요청하지는 않았는지 검사합니다. 빈값으로 요청했다면 에러 메시지를 보냅니다.
-//        if (postUserReq.getUserAddress().length() == 0) {
-//            return new BaseResponse<>(POST_USERS_EMPTY_ADDRESS);
-//        }
-//        // 관심사를 설정했는지, 빈 값으로 요청하지는 않았는지 검사합니다. 빈값으로 요청했다면 에러 메시지를 보냅니다.
-//        if (postUserReq.getUserLikes() == null) {
-//            return new BaseResponse<>(POST_USERS_EMPTY_LIKES);
-//        }
-//        try {
-//            PostUserRes postUserRes = userService.createUser(postUserReq);
-//            return new BaseResponse<>(postUserRes);
-//        } catch (BaseException exception) {
-//            return new BaseResponse<>((exception.getStatus()));
-//        }
+        //id 정규표현: 입력받은 id가 영문 대소문자,숫자 4-16자리 형식인지 검사합니다. 형식이 올바르지 않다면 에러 메시지를 보냅니다.
+        if (!isRegexId(joinUserReq.getId())) {
+            throw  new BaseException(BaseExceptionStatus.INVALID_ID);
+        }
+        // password에 값이 존재하는지, 빈 값으로 요청하지는 않았는지 검사합니다. 빈값으로 요청했다면 에러 메시지를 보냅니다.
+        if (joinUserReq.getPassword().length() == 0) {
+            throw new BaseException(BaseExceptionStatus.EMPTY_EMAIL);
+        }
+        //비밀번호 정규표현: 입력받은 비밀번호가 숫자, 특문 각 1회 이상, 영문은 대소문자 모두 사용하여 8~16자리 입력과 같은 형식인지 검사합니다. 형식이 올바르지 않다면 에러 메시지를 보냅니다.
+        if (!isRegexPassword(joinUserReq.getPassword())) {
+            throw new BaseException(INVALID_PASSWORD);
+        }
+        // 닉네임 값이 존재하는지, 빈 값으로 요청하지는 않았는지 검사합니다. 빈값으로 요청했다면 에러 메시지를 보냅니다.
+        if (joinUserReq.getName().length() == 0) {
+            throw new BaseException(BaseExceptionStatus.EMPTY_NICKNAME);
+        }
+        // 닉네임 정규표현: 입력받은 닉네임이 숫자와 영문, 한글로만 이루어졌는지 검사합니다. 형식이 올바르지 않다면 에러 메시지를 보냅니다.
+        if (!isRegexNickname(joinUserReq.getName())) {
+            throw new BaseException(BaseExceptionStatus.INVALID_NICKNAME);
+        }
 
         String uuid = UUID.randomUUID().toString();
         User user = User.builder()
